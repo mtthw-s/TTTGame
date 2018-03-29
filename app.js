cc22073b2476912c0cbcfae72960d60b729a499e
Matt <matt_sanford@outlook.com>
2018-03-29 04:53:08 +0000

    Basic server side rendering of a grid

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res, next) {  
-    res.render('index', { title: 'Hello World!' });
+    res.render('index', { title: 'Hello World!', boxes: [['Box1','Box2','Box3'],['Box4','Box5','Box6'],['Box7','Box8','Box9']] });
 });
 
-module.exports = app;  
+module.exports = app;
 
 
 
