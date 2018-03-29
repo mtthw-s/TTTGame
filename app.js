8ca9df722699fb66a660ce499b9d63d950dd12bc
Matt <matt_sanford@outlook.com>
2018-03-29 04:33:12 +0000

    added express, jade and basic rendering of the application

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');  
+const path = require('path');  
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));  
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res, next) {  
+    res.render('index', { title: 'Hello World!' });
+});
+
+module.exports = app;  
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+const app = require('./app');  
+const port = process.env.PORT || 3000;
 
-var http = require("http");
+app.listen(port, function() {  
+    console.log('Express server listening on port ' + port);
+});
+
+
+
+
+//var http = require("http");
 
 //Create HTTP server and listen on port 8000 for requests
-http.createServer(function (request, response) {
+//http.createServer(function (request, response) {
 
    // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   //response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = this.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
+   //response.end('Hello World\n');
+//}).listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  //var addr = this.address();
+  //console.log("Chat server listening at", addr.address + ":" + addr.port);
+//});
 
 // Print URL for accessing server
 //console.log('Server running at http://127.0.0.1:8000/')
